6c11dc3d217fc03cb8170047c1b64baa5755d461
Sanjeev Jha <sjha2@sastasundar.com>
2024-05-21 14:36:39 +0530

    initial

---
+++ application/config/routes.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+| This file lets you re-map URI requests to specific controller functions.
+|
+| Typically there is a one-to-one relationship between a URL string
+| and its corresponding controller class/method. The segments in a
+| URL normally follow this pattern:
+|
+|	example.com/class/method/id/
+|
+| In some instances, however, you may want to remap this relationship
+| so that a different class/function is called than the one
+| corresponding to the URL.
+|
+| Please see the user guide for complete details:
+|
+|	https://codeigniter.com/userguide3/general/routing.html
+|
+| -------------------------------------------------------------------------
+| RESERVED ROUTES
+| -------------------------------------------------------------------------
+|
+| There are three reserved routes:
+|
+|	$route['default_controller'] = 'welcome';
+|
+| This route indicates which controller class should be loaded if the
+| URI contains no data. In the above example, the "welcome" class
+| would be loaded.
+|
+|	$route['404_override'] = 'errors/page_missing';
+|
+| This route will tell the Router which controller/method to use if those
+| provided in the URL cannot be matched to a valid route.
+|
+|	$route['translate_uri_dashes'] = FALSE;
+|
+| This is not exactly a route, but allows you to automatically route
+| controller and method names that contain dashes. '-' isn't a valid
+| class or method name character, so it requires translation.
+| When you set this option to TRUE, it will replace ALL dashes in the
+| controller and method URI segments.
+|
+| Examples:	my-controller/index	-> my_controller/index
+|		my-controller/my-method	-> my_controller/my_method
+*/
+$route['default_controller'] = 'post';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+$route['blog/post/create'] = 'post/create';  // Route for creating a new post
+$route['blog/post/edit/(:any)'] = 'post/edit/$1';  // Route for editing a post, $1 will be the user ID
+$route['blog/post/delete/(:any)'] = 'post/delete/$1';  // Route for deleting a post, $1 will be the user ID
+$route['blog/post/(:any)'] = 'post/view/$1';  // Route for viewing a single post, $1 will be the user ID
+$route['blog/post'] = 'post';  // Route for listing all post
